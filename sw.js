0d4d1337cd38b36838b95a3871f6071101142e92
Hptrivia <rasheed-ayodeji.owolabi@iubh.de>
2026-03-15 17:37:01 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  // minimal fetch handler so Chrome sees a real service worker
+});
